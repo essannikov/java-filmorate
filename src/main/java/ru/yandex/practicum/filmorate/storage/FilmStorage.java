a482ed48e3f9844753209ffd3034a2ec8e9c7bda
Evgenii Sannikov <essannikov@gmail.com>
2026-05-28 01:20:29 +0300

    CheckStyle fix. Separated 2.0

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,9 +6,14 @@ import java.util.Collection;
 
 public interface FilmStorage {
     public Collection<Film> getAll();
+
     public Collection<Film> getPopular(Integer count);
+
     public Film get(Long id);
+
     public Film add(Film film);
+
     public Film update(Film newFilm);
+
     public Film delete(Long id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,8 +6,12 @@ import java.util.Collection;
 
 public interface UserStorage {
     public Collection<User> getAll();
+
     public User get(Long id);
+
     public User add(User User);
+
     public User update(User newUser);
+
     public User delete(Long id);
 }
